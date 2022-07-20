14472f4ce032592b67fb9889ee8541074c481327
Baljit998 <71004677+Baljit998@users.noreply.github.com>
2022-07-20 12:06:52 +0530

    commit

---
+++ class work/assignment.py
@@ -0,0 +1,10 @@
+a = 10
+ 
+b = a
+print(b)
+ 
+b += a
+print(b)
+ 
+b -= a
+print(b)
---
+++ class work/comparison.py
@@ -0,0 +1,13 @@
+a = 13
+b = 33
+print(a > b)
+
+print(a < b)
+
+print(a == b)
+
+print(a != b)
+
+print(a >= b)
+
+print(a <= b)
